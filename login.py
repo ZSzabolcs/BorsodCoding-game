bfc7431bb1980c29a14d3857379c513ee047876d
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-28 21:01:26 +0100

    Sikeres POST kérés

--- login.py
+++ login.py
@@ -1,29 +1,66 @@
 import tkinter
 from tkinter import ttk
+import requests
+import datetime
+
+def login(name_entry, passw_entry, URL, app, root):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    json_data = {
+        "name" : name_entry.get(),
+        "password" : passw_entry.get(),
+        "date" : timestamp
+    }
+
+    try:
+        response = requests.post(URL, json=json_data)
+
+        response.raise_for_status()
+
+        print("Sikeres kérés")
+        print(response.status_code)
+        app.successfull = True
+        data = response.json()
+        print("JSON válasz")
+        print(data)
+
+        if app.successfull:
+            root.destroy()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Hiba történt: {e}")
+
 
 
 def loginWindow():
-    root = tkinter.Tk()
-    root.geometry("300x200")
-    root.title("For The Potatoe login")
+    class App:
+        def __init__(self):
+           self.successfull = False
 
-    username_label = ttk.Label(root, text="Username:")
+    LOGIN_URL = "http://localhost:5233/api/UserRegistData/Login"
+
+    app = tkinter.Tk()
+    openedApp = App()
+    app.geometry("300x200")
+    app.title("For The Potatoe login")
+    app.protocol("WM_DELETE_WINDOW", app.destroy)
+
+    username_label = ttk.Label(app, text="Username:")
     username_label.pack(pady=2)
 
-    username_entry = ttk.Entry(root)
+    username_entry = ttk.Entry(app)
     username_entry.pack(pady=5)
     username_entry.focus()
 
-    password_label = ttk.Label(root, text="Password:")
+    password_label = ttk.Label(app, text="Password:")
     password_label.pack(pady=2)
 
-    password_entry = ttk.Entry(root, show="*")
+    password_entry = ttk.Entry(app, show="*")
     password_entry.pack(pady=5)
 
     login_button = ttk.Button(
-        root,
+        app,
         text="Login",
-        command=lambda: print("Működik")
+        command=lambda: login(username_entry, password_entry, LOGIN_URL, openedApp, app)
     )
 
     login_button.pack(
@@ -31,6 +68,6 @@ def loginWindow():
     )
 
 
-    root.mainloop()
-
+    app.mainloop()
+    return openedApp
 
